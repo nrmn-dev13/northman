03bf674070e3e276dbdba8d93e3cd7ad950acd35
Norman <normanramadhan3@gmail.com>
2021-02-18 19:01:14 +0700

    Adding scss

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import './style.css'
+import './css/style.scss'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
